2b3bfe169370d8a367f8f71123a1f46c6a0f7dbf
shahedc <shahedc@gmail.com>
2019-04-04 00:15:06 -0400

    added projects for Empty template and Mapped Branches

---
+++ MappedBranches/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MappedBranches
+{
+    public class Startup
+    {
+        private static void HandleBranch1(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Branch 1.");
+            });
+        }
+
+        private static void HandleBranch2(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Branch 2.");
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Map("/mappedBranch1", HandleBranch1);
+
+            app.Map("/mappedBranch2", HandleBranch2);
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Terminating Run() method.");
+            });
+        }
+    }
+}
